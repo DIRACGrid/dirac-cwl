f7b90acc8468acf553d00be783cae556c1c986d8
Stella-Maria Renucci <stellarenucci04@gmail.com>
2026-02-17 11:52:32 +0100

    docs: updated docstring

--- src/dirac_cwl/submission_models.py
+++ src/dirac_cwl/submission_models.py
@@ -175,25 +175,30 @@ class ProductionSubmissionModel(BaseModel):
         return values
 
 
-# ResourceRequirement validations, temporary code, waiting on cwltool PR: https://github.com/common-workflow-language/cwltool/pull/2179.
+# -----------------------------------------------------------------------------
+# ResourceRequirement validations
+# -----------------------------------------------------------------------------
+# Temporary code, waiting on cwltool PR: https://github.com/common-workflow-language/cwltool/pull/2179.
+
+
 def validate_resource_requirements(task):
     """
     Validate ResourceRequirements of a task (CommandLineTool, Workflow, WorkflowStep, WorkflowStep.run).
 
     :param task: The task to validate
     """
-    cwl_req = get_resource_requirement(task)
+    cwl_req = _get_resource_requirement(task)
 
     # Validate Workflow/CLT requirements.
     if cwl_req:
-        validate_resource_requirement(cwl_req)
+        _validate_resource_requirement(cwl_req)
 
     # Validate WorkflowStep requirements.
     if not isinstance(task, CommandLineTool) and task.steps:
         for step in task.steps:
-            step_req = get_resource_requirement(step)
+            step_req = _get_resource_requirement(step)
             if step_req:
-                validate_resource_requirement(step_req, cwl_req=cwl_req)
+                _validate_resource_requirement(step_req, cwl_req=cwl_req)
 
             # Validate run requirements for each step if they exist.
             if step.run:
@@ -201,27 +206,34 @@ def validate_resource_requirements(task):
                     # Validate nested Workflow requirements, if any.
                     validate_resource_requirements(task=step.run)
 
-                step_run_req = get_resource_requirement(step.run)
+                step_run_req = _get_resource_requirement(step.run)
                 if step_run_req:
-                    validate_resource_requirement(step_run_req, cwl_req=cwl_req)
+                    _validate_resource_requirement(step_run_req, cwl_req=cwl_req)
 
 
-def validate_resource_requirement(requirement, cwl_req=None):
+def _validate_resource_requirement(requirement, cwl_req=None):
     """Validate a ResourceRequirement.
 
     Verify:
      - that resourceMin is not higher than resourceMax (CommandLineTool, Workflow, WorkflowStep, WorkflowStep.run)
-     - that resourceMin (WorkflowStep, WorkflowStep.run) is not higher than global (Workflow) resourceMax.
+     - that resourceMin (WorkflowStep, WorkflowStep.run) is not higher than the Workflow-level resourceMax.
 
     :param requirement: The current ResourceRequirement to validate.
-    :param cwl_req: The global Workflow/CLT requirement, if any.
+    :param cwl_req: The Workflow-level/CLT requirement, if any.
     :raises ValueError: If the requirement is invalid.
     """
 
-    def check_resource(current_resource, req_min_value, req_max_value, global_max_value=None):
+    def _check_resource(current_resource, req_min_value, req_max_value, wf_req_max_value=None):
+        """Check single resource requirement values.
+
+        :param current_resource: The current checked resource (ram, cores, tmpdir, outdir).
+        :param req_min_value: The current resourceMin value.
+        :param req_max_value: The current resourceMax value.
+        :param wf_req_max_value: The Workflow-level resourceMax value, if any.
+        """
         if req_min_value and req_max_value and req_min_value > req_max_value:
             raise ValueError(f"{current_resource}Min is higher than {current_resource}Max")
-        if global_max_value and req_min_value and req_min_value > global_max_value:
+        if wf_req_max_value and req_min_value and req_min_value > wf_req_max_value:
             raise ValueError(f"{current_resource}Min is higher than global {current_resource}Max")
 
     for resource, min_value, max_value in [
@@ -230,7 +242,7 @@ def validate_resource_requirement(requirement, cwl_req=None):
         ("tmpdir", requirement.tmpdirMin, requirement.tmpdirMax),
         ("outdir", requirement.outdirMin, requirement.outdirMax),
     ]:
-        check_resource(
+        _check_resource(
             resource,
             min_value,
             max_value,
@@ -238,7 +250,7 @@ def validate_resource_requirement(requirement, cwl_req=None):
         )
 
 
-def get_resource_requirement(
+def _get_resource_requirement(
     cwl_object: Workflow | CommandLineTool | WorkflowStep,
 ) -> ResourceRequirement | None:
     """
